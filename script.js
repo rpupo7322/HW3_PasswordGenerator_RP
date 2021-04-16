d09d9d4c09e9bd80f21325d0b357e89fdbb374a1
Rob Pupo <pupo.robert@gmail.com>
2021-04-15 22:04:20 -0400

    replaced JS file with another, improved the password generation with validity

--- script.js
+++ script.js
@@ -22,75 +22,91 @@ submitBtn.addEventListener("click", submit);
 
 // Generate password function
 function generatePassword(len, up, low, num, spec) {
-  console.log(len);
-    var length = len;
+//   console.log(len);
+  var length = len;
   var characters = getCharacters(up, low, num, spec);
-    var retVal = "";
+  var pass = "";
   for (var i = 0, n = characters.length; i < length; ++i) {
-      retVal += characters.charAt(Math.floor(Math.random() * n));
+    pass += characters.charAt(Math.floor(Math.random() * n));
   }
-  console.log(retVal);
+  var passw = ensureValidity(pass, up, low, num, spec);
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = retVal;
-
-  return retVal;
+  passwordText.value = passw;
 
 }
 
+// replace a string with string.index(4) = newVal
 
-
-// click button > popup
-// hit submit button > input parameters into generate password
-
+function ensureValidity (pass, up, low, num, spec) {
+    var lockedUp = 129, lockedLow = 129, lockedNum = 129, lockedSpec = 129;
+    var pass2 = pass.split("");
+    console.log("pass2" , pass2)
+    // console.log("ensurevalidity:", pass, up, low, num, spec)
+    if (up) {
+        lockedUp = Math.floor(Math.random() * pass.length);
+        pass2[lockedUp] = charactersList[0].charAt(Math.floor(Math.random() * charactersList[0].length));
+    }
+    if (low) {
+        do {
+            lockedLow = Math.floor(Math.random() * pass.length)
+        } while (lockedLow === lockedUp)
+        pass2[lockedLow] = charactersList[1].charAt(Math.floor(Math.random() * charactersList[1].length))
+    }
+    if (num) {
+        do {
+            lockedNum = Math.floor(Math.random() * pass.length)
+        } while (lockedNum === lockedUp || lockedNum === lockedLow)
+        pass2[lockedNum] = charactersList[2].charAt(Math.floor(Math.random() * charactersList[2].length))
+    }
+    if (spec) {
+        do {
+            lockedSpec = Math.floor(Math.random() * pass.length)
+        } while (lockedSpec === lockedUp || lockedSpec === lockedLow || lockedSpec === lockedNum)
+        pass2[lockedSpec] = charactersList[3].charAt(Math.floor(Math.random() * charactersList[3].length))
+    }
+    console.log(pass2);
+    var pass3 = pass2.join("");
+    console.log("Upper:", lockedUp, "Lower:", lockedLow, "Numeric:", lockedNum, "Special:", lockedSpec)
+    return pass3;
+}
+var charactersList = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz",
+"0123456789", "!@#$%^&*"];
 
 // -----------------------------
-
 
 function popup () {
     var popwindow = document.getElementById("popup");
     popwindow.style.display = "block";
 
-    // document.getElementById("popup").style.display = 'auto';
-    console.log(popwindow);
 }
 
 function closePopup () {
     var popwindow = document.getElementById("popup");
     popwindow.style.display = "none";
-    console.log(popwindow);
 }
 
+
 function getCharacters(a, b, c, d) {
-    var charactersList = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz",
-         "0123456789", "!@#$%^&*"];
-    
     inputs = [a, b, c, d]
     chars = ''
-
     for(i = 0; i < charactersList.length; i++) {
         if (inputs[i]) {
             chars += charactersList[i];
         }
     }
-    console.log(chars);
     return chars;
 }
 
 function submit() {
     var len = document.getElementById("length").value;
-    // console.log(len);
     var upper = document.getElementById("upper").checked;
-    // console.log(len);
     var lower = document.getElementById("lower").checked;
-    // console.log(len);
     var number = document.getElementById("numbers").checked;
     var special = document.getElementById("special").checked;
 
-    console.log(len, upper, lower, special, number);
     generatePassword(len, upper, lower, number, special);
     closePopup();
-
 }
 
 
